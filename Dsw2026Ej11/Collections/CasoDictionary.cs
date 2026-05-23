5d159df2cbcca9746c910e894e49f47dead76b6d
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2026-05-22 21:47:59 -0300

    Ejercicios finalizados

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,49 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private Dictionary<int, Alumno>? _alumnos;
+
+    public CasoDictionary()
+    {
+        _alumnos = new Dictionary<int, Alumno>();
+    }
+
+    public void AgregarAlumno(Alumno? alumno)
+    {
+        if (alumno != null)
+        {
+            _alumnos?.Add(alumno.Id, alumno);
+        }
+    }
+
+    public Alumno? ObtenerAlumnoPorClave(int clave)
+    {
+        if (clave >= 0)
+        {
+            foreach (var item in _alumnos)
+            {
+                if(clave.Equals(item.Key))
+                {
+                    return item.Value;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Dictionary<int, Alumno>? ObtenerAlumnos()
+    {
+        return _alumnos;
+    }
+
+    public void EliminarAlumnoPorClave(int clave)
+    {
+        if (clave >= 0)
+        {
+            _alumnos?.Remove(clave);
+        }
+    }
+
+
+
 }
--- Dsw2026Ej11/Collections/CasoLinq.cs
+++ Dsw2026Ej11/Collections/CasoLinq.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 /*
  * Para cada punto crear un método que permita:
@@ -16,4 +18,69 @@
  */
 public class CasoLinq
 {
+    public CasoLinq()
+    {
+
+    }
+
+    // 1 OBTENER EL PRIMER LIBRO
+    public static Libro? GetPrimero(List<Libro> libros)
+    {
+        return libros.FirstOrDefault();
+    }
+
+    // 2 OBTENER EL ULTIMO LIBRO
+    public static Libro? GetUltimo(List<Libro> libros)
+    {
+        return libros.LastOrDefault();
+    }
+
+    // 3 OBTENER LA SUMA DE PRECIOS
+    public static decimal GetTotalPrecios(List<Libro> libros)
+    {
+        return libros.Sum(libro => libro.Precio);
+    }
+
+    // 4 OBTENER EL PROMEDIO DE PRECIOS
+    public static decimal GetPromedioPrecios(List<Libro> libros)
+    {
+        return libros.Average(libro => libro.Precio);
+    }
+
+    // 5 OBTENER LA LISTA DE LIBROS CON ID MAYOR A 15
+    public static List<Libro> GetListById(List<Libro> libros)
+    {
+        return libros.Where(libro => libro.Id > 15).ToList();
+    }
+
+    // 6 OBTENER UNA LISTA DE CADA LIBRO CON SU TÍTULO Y PRECIO EN FORMATO MONEDA
+    public static List<string> GetLibros(List<Libro> libros)
+    {
+        return libros.Select(libro => $"{libro.Titulo} - {libro.Precio:C}").ToList();
+    }
+
+    // 7 OBTENER EL LIBRO CON EL PRECIO MAS ALTO
+    public static Libro? GetMayorPrecio(List<Libro> libros)
+    {
+        return libros.OrderByDescending(libro => libro.Precio).FirstOrDefault();
+    }
+
+    // 8 OBTENER EL LIBRO CON EL PRECIO MAS BAJO
+    public static Libro? GetMenorPrecio(List<Libro> libros)
+    {
+        return libros.OrderBy(libro => libro.Precio).FirstOrDefault();
+    }
+
+    //9 OBTENER LOS LIBROS CUYO PRECIO SEA MAYOR AL PROMEDIO
+    public static List<Libro>? GetMayorPromedio(List<Libro> libros)
+    {
+        var promedio = GetPromedioPrecios(libros);
+        return libros.Where(libro => libro.Precio > promedio).ToList();
+    }
+
+    // 10 OBTENER LOS LIBROS ORDENADOS POR TÍTULO DE FORMA DESCENDENTE
+    public static List<Libro>? GetLibrosOrdenados(List<Libro> libros)
+    {
+        return libros.OrderByDescending(libro => libro.Titulo).ToList();
+    }
 }
